6a3a26df2522b328ab2c78dc12e343828f2e3549
Shaquaile <shaquaile.w@gmail.com>
2024-07-23 23:01:59 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ python manage.py collectstatic --noinput
 
 # Start uWSGI
 echo "Starting uWSGI..."
-exec uwsgi --socket :9000 --workers 4 --master --enable-threads --module storefront.wsgi --harakiri 60 --max-requests 5000 --vacuum --buffer-size 32768
+exec uwsgi --socket :9000 --workers 4 --master --enable-threads --module storefront.wsgi --harakiri 120 --max-requests 5000 --vacuum --buffer-size 65535 --logto /tmp/uwsgi.log
