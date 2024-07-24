783fa97ccde8e3a33f28470f7633fd6b80e1d9b9
Shaquaile <shaquaile.w@gmail.com>
2024-07-23 22:39:12 -0400

    fixed run.sh error added necessary environment variables

--- docker/proxy/run.sh
+++ docker/proxy/run.sh
@@ -6,7 +6,8 @@ set -e
 export host=\$host
 export request_uri=\$request_uri
 
-# Always use the non-SSL config
-envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+# Generate the configuration file from the template, specifying the environment variables to substitute
+envsubst '\$DOMAIN \$APP_HOST \$APP_PORT' < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 
+# Start Nginx
 nginx -g 'daemon off;'
