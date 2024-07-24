93e8c2a3489cf9dbd4f11739ad9ae0bd42837204
Shaquaile <shaquaile.w@gmail.com>
2024-07-23 23:06:11 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 # Applying database migrations
 echo "Applying database migrations..."
 python manage.py makemigrations
@@ -11,4 +13,4 @@ python manage.py collectstatic --noinput
 
 # Start uWSGI
 echo "Starting uWSGI..."
-exec uwsgi --socket :9000 --workers 4 --master --enable-threads --module storefront.wsgi --harakiri 120 --max-requests 5000 --vacuum --buffer-size 65535 --logto /tmp/uwsgi.log
+exec uwsgi --socket :9000 --workers 4 --master --enable-threads --module storefront.wsgi --harakiri 120 --max-requests 5000 --vacuum --buffer-size 65535 --logto /tmp/uwsgi.log --log-level debug
